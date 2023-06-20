9719af5f6ce297b00039aab3ede0a1e8995244c5
brycec23 <brycec23@vt.edu>
2023-06-20 13:01:39 -0400

    Initial Commit

---
+++ Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Bryce Cullen on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
